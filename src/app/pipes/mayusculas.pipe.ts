8d115c5828b3cec984712380b483a478e9098343
Alexander <alexander.martinez@lapatota.cl>
2022-06-20 17:48:35 -0400

    valor y argumenentos a los pipes personalizados

--- src/app/pipes/mayusculas.pipe.ts
+++ src/app/pipes/mayusculas.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class MayusculasPipe implements PipeTransform {
-    transform(value: string): string {
-        return 'Hola Mundo'
+    transform(value: string, enMayuscula: boolean = true): string {
+
+        // if (enMayuscula) {
+        //     return value.toUpperCase()
+        // }
+        // return value.toLowerCase()
+
+        return (enMayuscula)
+            ? value.toUpperCase()
+            : value.toLocaleLowerCase();
     }
 
 }
