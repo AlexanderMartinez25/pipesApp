c2fc9b3c7a4cd90915a1a2e7d9d5a45323ecfdce
Alexander <alexander.martinez@lapatota.cl>
2022-06-22 18:39:18 -0400

    Lenar una PrimeTable con data + vuelaPipe

--- src/app/primer-ng/primer-ng.module.ts
+++ src/app/primer-ng/primer-ng.module.ts
@@ -4,17 +4,17 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-import { ToolbarModule } from 'primeng/toolbar';
 import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
-    MenubarModule,
     FieldsetModule,
-    ToolbarModule,
-    TableModule
+    MenubarModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimerNgModule { }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,8 +10,37 @@ import { Component } from '@angular/core';
 export class OrdenarComponent {
 
   enMayuscula: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
 
   toggleString() {
+    console.log(this.heroes);
     this.enMayuscula = !this.enMayuscula
   }
 
